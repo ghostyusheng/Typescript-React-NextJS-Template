ae6db46391afbc87a98ccf927aefd99d708f1ea5
ghostyusheng <ghostyusheng@gmail.com>
2024-02-06 15:53:36 +0000

    init

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,9 +1,46 @@
 import Header from "../common/header/layout"
+import React from 'react';
+import { Button, Flex } from 'antd';
+import { Carousel } from 'antd';
+import { Layout } from 'antd';
+const { Footer, Sider, Content } = Layout;
+
+
+
+const contentStyle: React.CSSProperties = {
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
+
 
 export default function Page() {
     return (
-        <div className="bg-neutral-700">
+        <div className="bg-neutral-700 px-20">
         <Header></Header>
+        <Flex wrap="wrap" gap="small">
+            {Array.from({ length: 24 }, (_, i) => (
+            <Button key={i} type="primary">
+                Button
+            </Button>
+            ))}
+        </Flex>
+        <Carousel autoplay>
+            <div>
+            <h3 style={contentStyle}>1</h3>
+            </div>
+            <div>
+            <h3 style={contentStyle}>2</h3>
+            </div>
+            <div>
+            <h3 style={contentStyle}>3</h3>
+            </div>
+            <div>
+            <h3 style={contentStyle}>4</h3>
+            </div>
+        </Carousel>
         aaaaaaaaaa111</div>
     )
 }
