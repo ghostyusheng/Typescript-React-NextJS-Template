c1adb90bb3550d41606f8e18d3d99130d99867ed
ghostyusheng <ghostyusheng@gmail.com>
2024-02-06 12:48:15 +0000

    page header

---
+++ app/common/header/layout.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <>
+            <div className="h-40 border-black border-2 px-20">
+                <div className="mx-2 mt-2">Logo</div>
+            </div>
+        </>
+    )
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+import Header from "../common/header/layout"
+
+export default function Page() {
+    return (
+        <div className="bg-neutral-700">
+        <Header></Header>
+        aaaaaaaaaa111</div>
+    )
+}
